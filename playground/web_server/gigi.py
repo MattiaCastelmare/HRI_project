0c45be5516dd2b8a5743a8fb32927ca3750a82e7
mattiacastelmare <mattiacastelmare@gmail.com>
2024-02-09 18:58:37 +0100

    prova

--- playground/web_server/gigi.py
+++ playground/web_server/gigi.py
@@ -3,6 +3,6 @@ from pathlib import Path
 
 domain_file = Path("puzzle_domain.pddl")
 problem_file = Path("puzzle_problem.pddl")
-
+print("CIAO ANTONIO")
 plans = planners.plan_topk(domain_file=domain_file, problem_file=problem_file, number_of_plans_bound=100, timeout=30)
 print(plans)
